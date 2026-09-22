e176036788fb575c8b584a2ea9541c348f76025f
uhh <veraveetorres@gmail.com>
2026-09-22 16:29:52 +0900

    Added button which changes message to "Thanks for pressing."

--- app/src/main/java/com/example/myapplication1/MainActivity.kt
+++ app/src/main/java/com/example/myapplication1/MainActivity.kt
@@ -1,6 +1,8 @@
 package com.example.myapplication1
 
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +18,10 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun onButtonClick(view: android.view.View) {
+        val onetext = findViewById<TextView>(R.id.textView)
+        onetext.text = "Thanks for pressing."
     }
 }
